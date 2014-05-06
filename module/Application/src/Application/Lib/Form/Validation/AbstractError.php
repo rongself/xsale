43c6ff0a931c9b4be5c06bee21138638b5e08381
Ron Choi <roncai@crazysales.com.au>
2014-05-06 09:39:58 +0800

    add JsonResultModel

---
+++ module/Application/src/Application/Lib/Form/Validation/AbstractError.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-5-5
+ * Time: 下午1:46
+ */
+
+namespace Application\Lib\Form\Validation;
+
+
+abstract class AbstractError {
+
+} 
--- module/Application/src/Application/Lib/Form/Validation/Error.php
+++ module/Application/src/Application/Lib/Form/Validation/Error.php
@@ -9,6 +9,46 @@
 namespace Application\Lib\Form\Validation;
 
 
-class Error {
+class Error extends AbstractError{
 
+    private $field;
+    private $errorMessage;
+
+    public function __construct($field,$error)
+    {
+        $this->setField($field);
+        $this->setErrorMessage($error);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getErrorMessage()
+    {
+        return $this->errorMessage;
+    }
+
+    /**
+     * @param mixed $errorMessage
+     */
+    public function setErrorMessage($errorMessage)
+    {
+        $this->errorMessage = $errorMessage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    /**
+     * @param mixed $field
+     */
+    public function setField($field)
+    {
+        $this->field = $field;
+    }
 } 
